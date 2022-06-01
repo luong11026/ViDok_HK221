e400f7a7f0c370c3787c223555d9c837b61e8216
Nguyễn Quang Đức <nguyenquangduc2000@gmail.com>
2022-06-01 13:01:58 +0700

    Fix deployment

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,10 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 import os
 
-bind = '0.0.0.0:5005'
+bind = '0.0.0.0:10007'
 workers = os.cpu_count()
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
 timeout = 300
